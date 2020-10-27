cc35b759ed27037c4f7b818433efb182a39c759b
GS <aryh@ya.ru>
2020-10-27 10:14:51 +0300

    038 lesson. Работа с табами

---
+++ js/script.js
@@ -0,0 +1,38 @@
+"use strict";
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabContent() {
+        tabsContent.forEach(item => {
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabContent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (e) => {
+        if (e.target && e.target.classList.contains('tabheader__item')) {
+            tabs.forEach((tab, i) => {
+                if (e.target == tab) {
+                    hideTabContent();
+                    showTabContent(i);
+                }
+            });
+        }
+    });
+
+});
